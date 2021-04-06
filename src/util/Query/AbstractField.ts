e9fab261c974bd9cb875eef42c6c1cb86e28567d
yeegor <yegor.batov@gmail.com>
2021-04-06 19:25:33 +0300

    Improve taxonomy: less levels + no ignores

---
+++ src/util/Query/AbstractField.ts
@@ -0,0 +1,130 @@
+import { InlineFragment } from './InlineFragment';
+import type { HigherKindType, FieldDescendantStore } from '../types';
+import Field from './Field';
+
+export interface Argument {
+    name: string;
+    type: string;
+    value: string;
+}
+
+// Workaround. Should be improved when partial type inference is supported by TS.
+export type FetchedFieldItemType = string | number | null;
+
+/** @namespace Graphql/Util/Query/Field/Field */
+export abstract class AbstractField<Name extends string, FieldReturnType> {
+    /**
+     * Type of name is changeable by setting an alias onto it.
+     * The actual value of name is immutable.
+     */
+    readonly name: Name;
+    readonly tag: keyof FieldDescendantStore<any, any> = 'AbstractField';
+
+    alias = '';
+
+    children: Field<string, unknown>[] = [];
+
+    args: Argument[] = [];
+
+    resultTypeHolder: FieldReturnType = {} as FieldReturnType;
+
+    constructor(name: Name) {
+        this.name = name;
+    }
+
+    /**
+     * This function will change type of the Field such way that it'll seem that the name has changed.
+     * The name is immutable and therefore will not actually get changed.
+     * This illusion is implemented so that you have proper typings for the queries' return values
+     */
+    setAlias<Alias extends string>(alias: Alias): HigherKindType<
+        this['tag'], 
+        Alias, 
+        FieldReturnType
+    > {
+        this.alias = `${alias}:`;
+
+        return this as any;
+    }
+
+    addArgument(name: string, type: string, value: any) {
+        if (value === undefined) {
+            return this;
+        }
+
+        this.args.push({ name, type, value});
+
+        return this;
+    }
+
+    // ! DO NOT REORDER THESE OVERLOADS
+    // ! IT WILL MAKE ME MIX UP INLINE FRAGMENTS WITH FIELDS
+    // ERROR
+    addField(arg: never): never;
+
+    // STRING
+    addField<
+        NewField extends string
+    >(
+        field: NewField
+    ): HigherKindType<
+        this['tag'], 
+        Name, 
+        FieldReturnType & { [K in NewField]: FetchedFieldItemType }
+    > 
+
+    // INLINE FRAGMENT
+    addField<
+        NewFieldName extends string, 
+        NewFieldReturnType, 
+        NewField extends InlineFragment<NewFieldName, NewFieldReturnType>
+    >( 
+        field: NewField
+    ): HigherKindType<
+        this['tag'], 
+        Name, 
+        FieldReturnType & Partial<NewField['resultTypeHolder']>
+    >;
+
+    // FIELD
+    addField<
+        S extends string, 
+        IRT, 
+        F extends Field<S, IRT>
+    >(
+        field: F
+    ): HigherKindType<
+        this['tag'], 
+        Name, 
+        FieldReturnType & { [K in F['name']]: F['resultTypeHolder'] }
+    >;
+    // !
+
+    addField(field: unknown): unknown {
+        if (typeof field === 'string') {
+            this.children.push(new Field(field));
+        } else if (field instanceof Field) {
+            this.children.push(field);
+        } else {
+            throw new Error('Unknown field type!');
+        }
+
+        return this;
+    }
+
+    addFieldList<
+        NewField extends string
+    >(
+        fieldList: readonly NewField[]
+    ): HigherKindType<
+        this['tag'],
+        Name,
+        FieldReturnType & { [K in NewField]: FetchedFieldItemType }
+    > {
+        fieldList.forEach(this.addField.bind(this));
+
+        return this as any;
+    }
+}
+
+export default AbstractField;
--- src/util/Query/InlineFragment.ts
+++ src/util/Query/InlineFragment.ts
@@ -1,51 +1,11 @@
-import { FetchedFieldItemType, Field } from './Field';
+import AbstractField from './AbstractField';
 
 /** @namespace Graphql/Util/Query/InlineFragment/InlineFragment */
-export class InlineFragment<
-    N extends string,
-    RT
-> extends Field<N, RT> {
-    /**
-     * This is against Liskov substitution principle, but we are willing to strictly differentiate
-     * Between InlineFragment and Field. Although they share functionality, they are not interchangeable.
-     */
-    // @ts-expect-error see above
-    public readonly isField = false;
-
-    readonly isInlineFragment: boolean = true;
+export class InlineFragment<N extends string, RT> extends AbstractField<N, RT> {
+    readonly tag = 'InlineFragment';
 
     constructor(name: N) {
         super(`... on ${name}` as N);
-    }
-
-    // ERROR
-    addField(arg: never): never;
-
-    // STRING
-    addField<S extends string>(field: S): InlineFragment<
-        N,
-        RT & { [K in S]: FetchedFieldItemType }
-    >;
-
-    // FIELD
-    addField<S extends string, IRT, F extends Field<S, IRT>>(field: F): InlineFragment<
-        N,
-        RT & { [K in F['name']]: F['resultTypeHolder'] }
-    >;
-
-    addField(field: any): any {
-        if (field instanceof InlineFragment) {
-            throw new Error('Cannot add a Fragment on a Fragment!');
-        }
-
-        return super.addField(field);
-    }
-
-    // @ts-expect-error see comment for isField
-    setAlias<A extends string>(alias: A): InlineFragment<A, RT> {
-        this.alias = `${alias}:`;
-
-        return this as unknown as InlineFragment<A, RT>;
     }
 }
 

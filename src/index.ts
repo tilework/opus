5c8e1ee9c1364cdd792b1663e694248e22513b9c
yeegor <yegor.batov@gmail.com>
2021-04-13 14:04:58 +0300

    Add missing exports from the package

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import { Client } from "./client";
 
 export { default as Field } from './builder/Field';
 export { default as InlineFragment } from './builder/InlineFragment';
+export { default as Query } from './builder/Query';
+export { default as Mutation } from './builder/Mutation';
+export { default as Batch } from './builder/Batch';
 
 export default new Client();

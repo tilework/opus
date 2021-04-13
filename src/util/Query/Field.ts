b40097871eeccd43c47b1644e009225708b7d4b9
yeegor <yegor.batov@gmail.com>
2021-04-13 12:31:14 +0300

    Make Field descendants initialize as non-array

--- src/util/Query/Field.ts
+++ src/util/Query/Field.ts
@@ -1,7 +1,11 @@
 import AbstractField from './AbstractField';
 
 /** @namespace Graphql/Util/Query/Field/Field */
-export class Field<Name extends string, FieldReturnType, A extends boolean> extends AbstractField<Name, FieldReturnType, A> {
+export class Field<
+    Name extends string, 
+    FieldReturnType, 
+    IsArray extends boolean = false
+> extends AbstractField<Name, FieldReturnType, IsArray> {
     readonly tag = 'Field';
 }
 
--- src/util/Query/Mutation.ts
+++ src/util/Query/Mutation.ts
@@ -3,10 +3,10 @@ import { GraphQlRequestType } from "../Request/prepareDocument";
 import { IRequestable } from "./interface/IRequestable";
 
 class Mutation<
-    N extends string, 
-    RT, 
-    A extends boolean = false
-> extends AbstractField<N, RT, A> implements IRequestable {
+    Name extends string, 
+    FieldReturnType, 
+    IsArray extends boolean = false
+> extends AbstractField<Name, FieldReturnType, IsArray> implements IRequestable {
     readonly tag = 'Mutation';
 
     readonly type = GraphQlRequestType.Mutation;

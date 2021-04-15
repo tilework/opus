c216a7834161f3c7984f4f4bd121026dd99739e3
yeegor <yegor.batov@gmail.com>
2021-04-15 14:23:33 +0300

    Export client as non-default

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { Client } from "./client";
 
+export type { DataType } from './util/data-type';
 export { default as Field } from './builder/Field';
 export { default as InlineFragment } from './builder/InlineFragment';
 export { default as Query } from './builder/Query';
 export { default as Mutation } from './builder/Mutation';
 export { default as CombinedField } from './builder/CombinedField';
-export type { DataType } from './util/data-type';
-
-export default new Client();
+export const client = new Client();

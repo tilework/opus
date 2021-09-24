51c273545df7fb900af173340b34c70844bf2e35
Okabe Rintaro <ejnshtein@gmail.com>
2021-09-24 16:06:05 +0300

    in datatype util add support for field return type

--- src/__tests__/DataType.test.ts
+++ src/__tests__/DataType.test.ts
@@ -20,6 +20,16 @@ const combinedRequest = new CombinedField()
     .add(query)
     .add(anotherQuery)
 
+const fieldRequest =  new Field('currencyData')
+    .addField(
+        new Field('available_currencies_data')
+            .addFieldList([
+                'id',
+                'label',
+                'value'
+            ]))
+    .addField('current_currency_code');
+
 describe('type is properly extracted', () => {
     it('extracts type from query', () => {
         const returned: DataType<typeof query> = {} as any;
@@ -52,4 +62,17 @@ describe('type is properly extracted', () => {
             returned.person.name;
         } catch {}
     });
+
+    it('extracts type from a field request', () => {
+        const returned: DataType<typeof fieldRequest> = {} as any;
+
+        try {
+            returned.currencyData;
+            returned.currencyData.available_currencies_data;
+            returned.currencyData.available_currencies_data.id;
+            returned.currencyData.available_currencies_data.label;
+            returned.currencyData.available_currencies_data.value;
+            returned.currencyData.current_currency_code;
+        } catch {}
+    })
 })
--- src/util/data-type.ts
+++ src/util/data-type.ts
@@ -1,11 +1,10 @@
 import { CombinedField } from "../builder/CombinedField";
-import { Mutation } from "../builder/Mutation";
-import { Query } from "../builder/Query";
+import { AbstractField } from "../builder/AbstractField";
 import { DeepReadonly } from "./deep-readonly";
 
 export type DataType<
-    T extends Query<any, any, any> | Mutation<any, any, any> | CombinedField<any>
-> = T extends Query<infer N, infer RT, infer A> | Mutation<infer N, infer RT, infer A>
+    T extends AbstractField<any, any, any> | CombinedField<any>
+> = T extends AbstractField<infer N, infer RT, infer A>
     ? DeepReadonly<{[k in N]: A extends true ? RT[] : RT}>
     : T extends CombinedField<infer RT>
     ? DeepReadonly<RT>
